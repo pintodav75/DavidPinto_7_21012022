194f4ec5de13b372f4f5f6a656536f87f9be920e
david <da.pinto75@gmail.com>
2022-01-23 04:26:53 +0100

    mise en place de Sequelize

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,5 @@
 const bcrypt = require('bcrypt');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-    })
-    .catch(error => res.status(500).json({ error }));
+   
 };
